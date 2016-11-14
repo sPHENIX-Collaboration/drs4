49024b628867c0aa6eab596310503e1a92f0057c
Martin Purschke <purschke@bnl.gov>
2016-11-14 07:12:07 -0500

    mlp -- augmented the plugin with a new device name, "device_drs4_by_serialnumber".        This allows RCDAQ to address a particular board, and allows to read out        multiple boards, which was not possible before.        Also fixed the identify() information to properly list the new devices that        the plugin now defines.

--- drs_plugin.cc
+++ drs_plugin.cc
@@ -27,7 +27,7 @@ int drs_plugin::create_device(deviceblock *db)
 	{
 
 	  add_readoutdevice ( new daq_device_drs( eventtype,
-					      subid ));
+						  subid ));
 	  return 0;  // say "we handled this request" 
 	}
 
@@ -37,8 +37,9 @@ int drs_plugin::create_device(deviceblock *db)
 
 
 	  add_readoutdevice ( new daq_device_drs( eventtype,
-					      subid,
-					      trigger ));
+						  subid,
+						  0,
+						  trigger ));
 	  return 0;  // say "we handled this request" 
 	}
 
@@ -48,8 +49,9 @@ int drs_plugin::create_device(deviceblock *db)
 	  float th = strtof ( db->argv4, 0);
 
 	  add_readoutdevice ( new daq_device_drs( eventtype,
-					      subid,
-					      trigger, th ));
+						  subid,
+						  0,
+						  trigger, th ));
 	  return 0;  // say "we handled this request" 
 	}
       else if ( db->npar == 6)
@@ -74,8 +76,9 @@ int drs_plugin::create_device(deviceblock *db)
 	    }
 
 	  add_readoutdevice ( new daq_device_drs( eventtype,
-					      subid,
-					      trigger, th, slope ));
+						  subid,
+						  0,
+						  trigger, th, slope ));
 	  return 0;  // say "we handled this request" 
 	}
 
@@ -106,8 +109,9 @@ int drs_plugin::create_device(deviceblock *db)
 
 
 	  add_readoutdevice ( new daq_device_drs( eventtype,
-					      subid,
-					      trigger, th, slope, delay ));
+						  subid,
+						  0,
+						  trigger, th, slope, delay ));
 	  return 0;  // say "we handled this request" 
 	}
 
@@ -136,8 +140,9 @@ int drs_plugin::create_device(deviceblock *db)
 	  int speed = get_value ( db->argv7);
 
 	  add_readoutdevice ( new daq_device_drs( eventtype,
-					      subid,
-					      trigger, th, slope, delay, speed ));
+						  subid,
+						  0,
+						  trigger, th, slope, delay, speed ));
 	  return 0;  // say "we handled this request" 
 	}
 
@@ -167,7 +172,8 @@ int drs_plugin::create_device(deviceblock *db)
 	  int startch = get_value ( db->argv8);
 
 	  add_readoutdevice ( new daq_device_drs( eventtype,
-					      subid,
+						  subid,
+						  0,
 						  trigger, th, slope,
 						  delay, speed, startch ));
 	  return 0;  // say "we handled this request" 
@@ -200,19 +206,306 @@ int drs_plugin::create_device(deviceblock *db)
 	  int nch = get_value ( db->argv9);
 
 	  add_readoutdevice ( new daq_device_drs( eventtype,
-					      subid,
+						  subid,
+						  0,
 						  trigger, th, slope,
 						  delay, speed, startch, nch ));
 	  return 0;  // say "we handled this request" 
 	}
 
+      else if ( db->npar == 11)
+	{
+	  int trigger = get_value ( db->argv3);
+	  float th = strtof ( db->argv4, 0);
 
+	  // slope can be 0 (negative) or 1 (positive)
+	  // or P / p or N / n 
+	  int slope = 0;
+	  if ( db->argv5[0] == 'P' || db->argv5[0] == 'p' )
+	    {
+	      slope = 0;
+	    }
+	  else if ( db->argv5[0] == 'N' || db->argv5[0] == 'n' )
+	    {
+	      slope = 1;
+	    }
+	  else
+	    {
+	      slope = get_value ( db->argv5);
+	    }
+
+	  int delay = get_value ( db->argv6);
+	  int speed = get_value ( db->argv7);
+	  int startch = get_value ( db->argv8);
+	  int nch = get_value ( db->argv9);
+	  int baseline = get_value ( db->argv10);
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  0,
+						  trigger, th, slope,
+						  delay, speed, startch, nch, baseline ));
+	  return 0;  // say "we handled this request" 
+	}
+
+
+
+      
       else
 	{
 	  return 1; // say it is our device but the parameters are wrong 
 	}
-    }  
+    } 
 
+
+  // the new "by serialnumber" device
+
+
+  else if ( strcasecmp(db->argv0,"device_drs_by_serialnumber") == 0 ) 
+    {
+      // we need at least 4 params
+      if ( db->npar <4 ) return 1; // indicate wrong params
+      
+      eventtype  = get_value ( db->argv1); // event type
+      subid = get_value ( db->argv2); // subevent id
+      int serialnumber = get_value ( db->argv3); // serialnumber
+
+      if ( db->npar == 4)
+	{
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber ));
+	  return 0;  // say "we handled this request" 
+	}
+
+      else if ( db->npar == 5)
+	{
+	  int trigger = get_value ( db->argv4);
+
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber,
+						  trigger ));
+	  return 0;  // say "we handled this request" 
+	}
+
+      else if ( db->npar == 6)
+	{
+	  int trigger = get_value ( db->argv4);
+	  float th = strtof ( db->argv5, 0);
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber,
+						  trigger, th ));
+	  return 0;  // say "we handled this request" 
+	}
+      
+      else if ( db->npar == 7)
+	{
+	  int trigger = get_value ( db->argv4);
+	  float th = strtof ( db->argv5, 0);
+	  
+	  // slope can be 0 (negative) or 1 (positive)
+	  // or P / p or N / n 
+	  int slope = 0;
+	  if ( db->argv6[0] == 'P' || db->argv6[0] == 'p' )
+	    {
+	      slope = 0;
+	    }
+	  else if ( db->argv6[0] == 'N' || db->argv6[0] == 'n' )
+	    {
+	      slope = 1;
+	    }
+	  else
+	    {
+	      slope = get_value ( db->argv6);
+	    }
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber,
+						  trigger, th, slope ));
+	  return 0;  // say "we handled this request" 
+	}
+
+
+      else if ( db->npar == 8)
+	{
+	  int trigger = get_value ( db->argv4);
+	  float th = strtof ( db->argv5, 0);
+
+	  // slope can be 0 (negative) or 1 (positive)
+	  // or P / p or N / n 
+	  int slope = 0;
+	  if ( db->argv6[0] == 'P' || db->argv6[0] == 'p' )
+	    {
+	      slope = 0;
+	    }
+	  else if ( db->argv6[0] == 'N' || db->argv6[0] == 'n' )
+	    {
+	      slope = 1;
+	    }
+	  else
+	    {
+	      slope = get_value ( db->argv6);
+	    }
+
+
+	  int delay = get_value ( db->argv7);
+
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber,
+						  trigger, th, slope, delay ));
+	  return 0;  // say "we handled this request" 
+	}
+
+      else if ( db->npar == 9)
+	{
+	  int trigger = get_value ( db->argv4);
+	  float th = strtof ( db->argv5, 0);
+
+	  // slope can be 0 (negative) or 1 (positive)
+	  // or P / p or N / n 
+	  int slope = 0;
+	  if ( db->argv6[0] == 'P' || db->argv6[0] == 'p' )
+	    {
+	      slope = 0;
+	    }
+	  else if ( db->argv6[0] == 'N' || db->argv6[0] == 'n' )
+	    {
+	      slope = 1;
+	    }
+	  else
+	    {
+	      slope = get_value ( db->argv6);
+	    }
+
+	  int delay = get_value ( db->argv7);
+	  int speed = get_value ( db->argv8);
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber,
+						  trigger, th, slope, delay, speed ));
+	  return 0;  // say "we handled this request" 
+	}
+
+      else if ( db->npar == 10)
+	{
+	  int trigger = get_value ( db->argv4);
+	  float th = strtof ( db->argv5, 0);
+
+	  // slope can be 0 (negative) or 1 (positive)
+	  // or P / p or N / n 
+	  int slope = 0;
+	  if ( db->argv6[0] == 'P' || db->argv6[0] == 'p' )
+	    {
+	      slope = 0;
+	    }
+	  else if ( db->argv6[0] == 'N' || db->argv6[0] == 'n' )
+	    {
+	      slope = 1;
+	    }
+	  else
+	    {
+	      slope = get_value ( db->argv6);
+	    }
+
+	  int delay = get_value ( db->argv7);
+	  int speed = get_value ( db->argv8);
+	  int startch = get_value ( db->argv9);
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber,
+						  trigger, th, slope,
+						  delay, speed, startch ));
+	  return 0;  // say "we handled this request" 
+	}
+
+      else if ( db->npar == 11)
+	{
+	  int trigger = get_value ( db->argv4);
+	  float th = strtof ( db->argv5, 0);
+
+	  // slope can be 0 (negative) or 1 (positive)
+	  // or P / p or N / n 
+	  int slope = 0;
+	  if ( db->argv6[0] == 'P' || db->argv6[0] == 'p' )
+	    {
+	      slope = 0;
+	    }
+	  else if ( db->argv6[0] == 'N' || db->argv6[0] == 'n' )
+	    {
+	      slope = 1;
+	    }
+	  else
+	    {
+	      slope = get_value ( db->argv6);
+	    }
+
+	  int delay = get_value ( db->argv7);
+	  int speed = get_value ( db->argv8);
+	  int startch = get_value ( db->argv9);
+	  int nch = get_value ( db->argv10);
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber,
+						  trigger, th, slope,
+						  delay, speed, startch, nch ));
+	  return 0;  // say "we handled this request" 
+	}
+
+      else if ( db->npar == 12)
+	{
+	  int trigger = get_value ( db->argv4);
+	  float th = strtof ( db->argv5, 0);
+
+	  // slope can be 0 (negative) or 1 (positive)
+	  // or P / p or N / n 
+	  int slope = 0;
+	  if ( db->argv6[0] == 'P' || db->argv6[0] == 'p' )
+	    {
+	      slope = 0;
+	    }
+	  else if ( db->argv6[0] == 'N' || db->argv6[0] == 'n' )
+	    {
+	      slope = 1;
+	    }
+	  else
+	    {
+	      slope = get_value ( db->argv6);
+	    }
+
+	  int delay = get_value ( db->argv7);
+	  int speed = get_value ( db->argv8);
+	  int startch = get_value ( db->argv9);
+	  int nch = get_value ( db->argv10);
+	  int baseline = get_value ( db->argv11);
+
+	  add_readoutdevice ( new daq_device_drs( eventtype,
+						  subid,
+						  serialnumber,
+						  trigger, th, slope,
+						  delay, speed, startch, nch, baseline ));
+	  return 0;  // say "we handled this request" 
+	}
+
+
+
+      
+      else
+	{
+	  return 1; // say it is our device but the parameters are wrong 
+	}
+    } 
+  
   return -1; // say " this is not our device"
 }
 
@@ -226,7 +519,9 @@ void drs_plugin::identify(std::ostream& os, const int flag) const
   else
     {
       os << " - DRS Plugin, provides - " << std::endl;
-      os << " -     device_drs (evttype, subid, triggerchannel, triggerthreshold[mV], slope[n/p], delay[ns], speed) - DRS4 Eval Board " << std::endl;
+      os << " -     device_drs (evttype, subid, triggerchannel, triggerthreshold[mV], slope[n/p], delay[ns], speed, start_ch, nch, baseline[mV]) - DRS4 Eval Board " << std::endl;
+      os << " -     device_drs_by_serialnumber ";
+      os << "                  (evttype, subid, serialnumber, triggerchannel, triggerthreshold[mV], slope[n/p], delay[ns], speed, start_ch, nch, baseline[mV]) - DRS4 Eval Board " << std::endl;
     }
       
 
